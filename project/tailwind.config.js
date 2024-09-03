5c2738d42dd2936f353f28fffd4b0611c201afba
Gabriel Ferreira <113285710+GendPh@users.noreply.github.com>
2024-09-03 19:53:24 +0100

    a

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -8,26 +8,34 @@ module.exports = {
 
   theme: {
     extend: {
-      colors:{
-        text:{
-          light:"#0c0e16",
-          dark:"#ffffff",
-          secondary:{
-            light:"#888eb0",
-            dark:"#dfe3fa"
+      colors: {
+        text: {
+          light: "#0c0e16",
+          dark: "#ffffff",
+          secondary: {
+            light: "#888eb0",
+            dark: "#dfe3fa"
+          },
+          btn: {
+            light: "#7E88C3",
+            dark: "#888EB0"
           }
         },
-        background:{
-          light:"#f8f8fb",
-          dark:"#141625",
+        background: {
+          light: "#f8f8fb",
+          dark: "#141625",
           header: "#373b53",
-          "new-invoice":{
-            light:"#ffffff",
-            dark:"#141625"
+          "new-invoice": {
+            light: "#ffffff",
+            dark: "#141625"
           },
-          input:{
-            light:"#ffffff",
-            dark:"#252945"
+          input: {
+            light: "#ffffff",
+            dark: "#252945"
+          },
+          btn: {
+            light: "#F9FAFE",
+            dark: "#252945"
           },
         },
 
